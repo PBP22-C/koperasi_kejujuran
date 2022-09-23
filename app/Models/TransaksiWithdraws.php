e2affa84b4b01a2b4a15ba38c4b55c9bd5ddf5bd
liemroym <liemroymarcelino@gmail.com>
2022-09-23 15:15:49 +0700

    -

--- app/Models/TransaksiWithdraws.php
+++ app/Models/TransaksiWithdraws.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class transaksi_withdraw extends Model
+class TransaksiWithdraws extends Model
 {
     use HasFactory;
     protected $table = 'transaksi_withdraws';
--- app/Models/barang.php
+++ app/Models/barang.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class barang extends Model
+class Barang extends Model
 {
     use HasFactory;
 
