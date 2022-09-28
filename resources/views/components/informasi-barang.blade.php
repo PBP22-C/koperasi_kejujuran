cc80980dbd99ac3ae9ac6f00f56fce5897e4f36b
Farrel Andhika Rizky Putra <farrelarp@students.undip.ac.id>
2022-09-28 16:16:52 +0700

    fix desc barang

--- resources/views/components/informasi-barang.blade.php
+++ resources/views/components/informasi-barang.blade.php
@@ -1,9 +1,9 @@
-<div id="informasi-barang" style="display: none;" class="bg-dark p-2">
+<div id="informasi-barang" style="display: none;" class="bg-dark p-2 card">
     <h1>Deskripsi Barang</h1>
-    <img src="" id="foto-barang" class="card-img-top text-white" alt="" height="250" style="object-fit:cover;">
+    <img src="" id="foto-barang" class="text-white" alt="" height="250" width="300" style="object-fit:contain;">
     <h2 id="nama-barang" class=""></h2>
     <p id="deskripsi-barang"></p>
     <div id="stok" class="text-secondary"></div>
-    <h2 id="harga-barang"></h1>
-    <button id="beli">Beli</button>
+    <h2 id="harga-barang" class="mb-2"></h2>
+    <button id="beli" class="btn btn-warning text-white"><i class="fa-solid fa-cart-shopping me-1"></i><span>Beli</span></button>
 </div>
