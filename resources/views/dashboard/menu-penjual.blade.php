3a32c1812bf060027aa6f638bfbe01de8d8d5ab9
alvintriseptia <alvintryseptia@gmail.com>
2022-10-03 05:13:47 +0700

    hide search bar in menu-penjual

--- resources/views/dashboard/menu-penjual.blade.php
+++ resources/views/dashboard/menu-penjual.blade.php
@@ -1,14 +1,5 @@
 <x-layout>
-    <div class="container row">
-        <div class="col-6">
-            <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                aria-describedby="button-addon2">
-        </div>
-        <div class="col-6 my-auto">
-            <i class="fa-solid fa-bell col-6"></i>
-        </div>
-    </div>
-    <div class="container my-5">
+    <div class="container">
         <div class="d-flex flex-wrap align-items-center gap-3 mb-4 bg">
             <h1 class="text-white justify-content-between me-auto">List Barang</h1>
             <button onclick="showTambah()" type="button" class="btn btn-primary" data-bs-toggle="modal"
